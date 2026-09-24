483d7135d256e5994cff09b19493c98c409feafd
Emilija <dudkoemilija@gmail.com>
2026-09-24 21:49:54 +0100

    currently blank blue grid

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+pygame.init()
+dark_blue = (44,44,127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock() 
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
